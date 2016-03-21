34040a0c8490e41f3dc598a2ac4c7d5baf5118bb
Umesh Chaurasiya <chaurasiyaumesh9@gmail.com>
2016-03-21 23:12:48 +0530

    commit - 22

--- app/frontend/assets/scripts/app.js
+++ app/frontend/assets/scripts/app.js
@@ -17,18 +17,36 @@ cartApp.config(function( $routeProvider, $locationProvider ) {
 		
 });
 
-cartApp.directive('slickSlider', function () {
+cartApp.directive('slickSlider', function ($timeout) {
     return {
         restrict: 'A',
         scope: {'data': '='},
         link: function (scope, element, attrs) {
             var isInitialized = false;
             scope.$watch('data', function(newVal, oldVal) {
-                if (newVal.length > 0 && !isInitialized) {
-                    $(element).slick(scope.$eval(attrs.slickSlider));
-                    isInitialized = true;
+				//console.log('newVal :',newVal);
+				//console.log('oldVal :',oldVal	);
+                if ( newVal.length > 0 && !isInitialized) {
+					//console.log('true',scope.$eval(attrs.slickSlider));
+					$timeout(function() {
+						$(element).slick( scope.$eval(attrs.slickSlider));
+						isInitialized = true;
+					},0);
+                    
                 }
             });
         }
     }
 });
+
+/*
+cartApp.directive('slickSlider',function($timeout){
+ return {
+   restrict: 'A',
+   link: function(scope,element,attrs) {
+     $timeout(function() {
+         $(element).slick(scope.$eval(attrs.slickSlider));
+     });
+   }
+ }
+}); */
