15f33a3221c1628de443e38de6f88b8fec4955af
Umesh Chaurasiya <umesh.chaurasiya@adapty.com>
2016-04-21 14:29:33 +0530

    21/4/2016 v0.2

--- app/backend/assets/scripts/controllers/attributeSetsCtrl.js
+++ app/backend/assets/scripts/controllers/attributeSetsCtrl.js
@@ -1,45 +1,81 @@
-adminApp.controller('attributeSetsCtrl', function($scope, $http, $routeParams, attributeService, $timeout ){
+adminApp.controller('attributeSetsCtrl', function($scope, $http, $routeParams, attributeService, attributeSetsService, $timeout ){
 	$scope.message = "Make New Attribute Set";
-	$scope.set = { attributesChosen:[], attributesAvailable:[]};
+	
+	//$scope.loadDefaults();
+	
 
-	getAllAttributes();
+	$scope.loadDefaults = function(){
+		$scope.set = { name:null, attributes:[] };// we'll be posting updated set of this data via submit form //attributeSet = {}
+		$scope.attributesAvailable = [];
+		getAllAttributes();
+	}
 
+	
 
+	
 	function getAllAttributes(){
 		attributeService.getAllAttributes().then( function( response ){
-			$scope.set.attributesAvailable = response;
+			$scope.attributesAvailable = response;
 		}, function( errorMessage ){
 			console.warn( errorMessage );
 		});
 	}
 
 	$scope.updateAttributesSelection = function( at ) {
-		if ( $scope.set.attributesChosen.indexOf( at ) == -1 && at.selected )
+		if ( $scope.set.attributes.indexOf( at ) == -1 && at.selected )
 		{
-			$scope.set.attributesChosen.push( at );
+			$scope.set.attributes.push( at );
 		}else {
-			var i = $scope.set.attributesChosen.indexOf( at );
-			$scope.set.attributesChosen.splice( i,1 );
+			var i = $scope.set.attributes.indexOf( at );
+			$scope.set.attributes.splice( i,1 );
 		}
 	}
 
 	$scope.selectAll = function(){
-		for ( var x=0; x< $scope.set.attributesAvailable.length; x++ )
+		for ( var x=0; x< $scope.attributesAvailable.length; x++ )
 		{
-			$scope.set.attributesAvailable[x].selected = true;
+			$scope.attributesAvailable[x].selected = true;
 		}
-		$scope.set.attributesChosen = angular.copy( $scope.set.attributesAvailable );
+		$scope.set.attributes = angular.copy( $scope.attributesAvailable );
 	}
 
 	$scope.deSelectAll = function(){
-		$scope.set.attributesChosen = [];
-		for ( var x=0; x< $scope.set.attributesAvailable.length; x++ )
+		$scope.set.attributes = [];
+		for ( var x=0; x< $scope.attributesAvailable.length; x++ )
 		{
-			$scope.set.attributesAvailable[x].selected = false;
+			$scope.attributesAvailable[x].selected = false;
 		}
 	}
 
-	$scope.addNewAttributeSet = function( set ){
-		// post request to server end point
+	$scope.addNew = function( set ){
+		attributeSetsService.addNew( set ).then( function( response ){
+			$scope.success = true;
+			$scope.loadDefaults();
+		}, function( errorMessage ){
+			console.warn( errorMessage );
+		});
 	}
+	$scope.getAll = function( ){
+		$scope.loading = true;
+		attributeSetsService.getAll( ).then( function( response ){
+			$scope.loading = false;
+			$scope.attributeSets = response;
+		}, function( errorMessage ){
+			console.warn( errorMessage );
+		});
+	}
+
+	function getAllSets(){
+		$scope.getAll();
+	}
+	$scope.getOneById = function( ){
+		
+	}
+	$scope.updateOneById = function( ){
+		
+	}
+	$scope.deleteOneById = function( ){
+		
+	}
+	getAllSets();
 });
--- routes/backend/backend.js
+++ routes/backend/backend.js
@@ -6,6 +6,7 @@ var appconfig = require('../../config/appconfig');
 var productsRoute = require('./products')( router );
 var categoriesRoute = require('./categories')( router);
 var attributesRoute = require('./attributes')( router);
+var attributeSetsRoute = require('./attributeSets')( router);
 
 
 module.exports = router;
