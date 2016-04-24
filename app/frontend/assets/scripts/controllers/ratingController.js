69247dd861e875e42aa13d7a097790064edb7f83
Umesh Chaurasiya <chaurasiyaumesh9@gmail.com>
2016-04-24 08:12:50 -0700

    24/4/2016 v0.6

--- app/frontend/assets/scripts/controllers/ratingController.js
+++ app/frontend/assets/scripts/controllers/ratingController.js
@@ -1,10 +1,26 @@
 cartApp.controller('ratingController', function( $scope ){
-	this.rating1 = 5;
+	/*this.rating1 = 5;
     this.rating2 = 1;
 	this.averageRating = 2;
     this.isReadonly = true;
     this.rateFunction = function(rating) {
       //console.log('Rating selected: ' + rating);
     };
-	
+	*/
+
+		$scope.user1 = {rating:5};
+	  $scope.user2 = {rating:2};
+	  $scope.user3 = {rating:1};  
+	  $scope.averageRating = 0;
+	  
+	  $scope.$watch(function(){return $scope.user1.rating + $scope.user2.rating + $scope.user3.rating;}, function(oldVal, newVal) {
+			if (newVal) { updateAverageRating(); }
+	  });
+		
+	  function updateAverageRating(){ $scope.averageRating = ($scope.user1.rating + $scope.user2.rating + $scope.user3.rating) / 3; }
+	  
+	  //$scope.isReadonly = true;
+	  $scope.rateFunction = function(rating) {
+		console.log("Rating selected: " + rating);
+	  };
 });
--- app/frontend/assets/scripts/directives/starRating.js
+++ app/frontend/assets/scripts/directives/starRating.js
@@ -1,3 +1,4 @@
+/*
 cartApp.directive('starRating', function () {
     return {
       restrict: 'EA',
@@ -42,3 +43,69 @@ cartApp.directive('starRating', function () {
       }
     };
   });
+
+  */
+
+ cartApp.directive("starRating", function() {
+  return {
+    restrict : "EA",
+    templateUrl:"./templates/star-rating.html",
+    scope : {
+      ratingValue : "=ngModel",
+      max : "=?", //optional: default is 5
+      onRatingSelected : "&?",
+      readonly: "=?"
+    },
+    link : function(scope, elem, attrs) {
+		 if ( typeof scope.onRatingSelected !== "function" )
+		  {
+			scope.onRatingSelected = function( rating ){};
+		  }
+      if (scope.max == undefined) { scope.max = 5; }
+      function updateStars() {
+        scope.stars = [];
+        for (var i = 0; i < scope.max; i++) {
+          scope.stars.push({
+            filled : (i < scope.ratingValue.rating)
+          });
+        }
+      };
+      scope.toggle = function(index) {
+        if (scope.readonly == undefined || scope.readonly == false){
+          scope.ratingValue.rating = index + 1;
+          scope.onRatingSelected({
+            rating: index + 1
+          });
+        }
+      };
+      scope.$watch("ratingValue.rating", function(oldVal, newVal) {
+        if (newVal) { updateStars(); }
+      });
+    }
+  };  
+});
+
+cartApp.directive("averageStarRating", function() {
+  return {
+    restrict : "EA",
+   templateUrl:"./templates/average-star-rating.html",
+    scope : {
+      averageRatingValue : "=ngModel",
+      max : "=?", //optional: default is 5
+    },
+    link : function(scope, elem, attrs) {
+      if (scope.max == undefined) { scope.max = 5; }
+      function updateStars() {
+        scope.stars = [];
+        for (var i = 0; i < scope.max; i++) {
+          scope.stars.push({});
+        }
+        var starContainerMaxWidth = 100; //%
+        scope.filledInStarsContainerWidth = scope.averageRatingValue / scope.max * starContainerMaxWidth;
+      };
+      scope.$watch("averageRatingValue", function(oldVal, newVal) {
+        if (newVal) { updateStars(); }
+      });
+    }
+  };
+});
