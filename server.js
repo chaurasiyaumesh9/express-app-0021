c3c47c3b6dbe60a7fb46598a9cb9e04ff2d1132a
Umesh Chaurasiya <umesh.chaurasiya@adapty.com>
2016-05-18 11:47:44 +0530

    18052016 v0.1

--- server.js
+++ server.js
@@ -22,32 +22,6 @@ process.env.NODE_PATH =  path.join( __dirname + "/") ;
 process.env.UPLOAD_PATH =  path.join( __dirname + "/uploads/") ;
 //process.env.PRERENDER_SERVICE_URL = "http://service.prerender.io";
 
-var arr = [
-   'googlebot',
-  // 'yahoo',
-  // 'bingbot',
-  'baiduspider',
-  'facebookexternalhit',
-  'twitterbot',
-  'rogerbot',
-  'linkedinbot',
-  'embedly',
-  'quora link preview',
-  'showyoubot',
-  'outbrain',
-  'pinterest/0.',
-  'developers.google.com/+/web/snippet',
-  'slackbot',
-  'vkShare',
-  'W3C_Validator',
-  'redditbot',
-  'Applebot',
-  'WhatsApp',
-  'flipboard'
-];
-
-
-
 //app.use(preRendered.set('prerenderServiceUrl', 'http://service.prerender.io'));//.set('prerenderToken', 'fnu9gwOPhdT0b30IXLI8'));
 //app.use(preRendered.set('prerenderServiceUrl', 'http://localhost:3000'));
 app.use(preRendered.set('prerenderServiceUrl', 'https://prerender-test402.herokuapp.com'));
@@ -58,7 +32,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()) // parse application/json
 app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
 
-app.use(session({ secret: 'meanstack402' })); // session secret
+//app.use(session({ secret: 'meanstack402' })); // session secret
 app.use(express.static( path.join(__dirname + '/app')));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
@@ -67,11 +41,13 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 app.use('/bower_components',  express.static(path.join(__dirname + '/bower_components')));
 app.use("/uploads", express.static(__dirname + '/uploads'));
 
-app.use('/admin', backend);  //accessing /admin will get you to route backend
-app.use('/admin', express.static(__dirname + '/app/backend') ); //go to static directoy backend while accessing /admin from url
 
-app.use('/', frontend); //accessing / will get you to frontend route
+app.use('/admin', express.static(__dirname + '/app/backend') ); //go to static directoy backend while accessing /admin from url
+app.use('/admin', backend);  //accessing /admin will get you to route backend
+
 app.use('/', express.static(__dirname + '/app/frontend') ); //go to static directoy fronend while accessing / from url
+app.use('/', frontend); //accessing / will get you to frontend route
+
 
 //app.use(preRendered.set('prerenderServiceUrl', 'https://prerender-test402.herokuapp.com/').set('prerenderToken', 'fnu9gwOPhdT0b30IXLI8'));
 
