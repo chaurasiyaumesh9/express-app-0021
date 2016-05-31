630dad22b39846f930af9365ab833d11be84a51e
Umesh Chaurasiya <umesh.chaurasiya@adapty.com>
2016-05-31 14:25:55 +0530

    31052016 v0.1

--- app/frontend/assets/scripts/controllers/cartCtrl.js
+++ app/frontend/assets/scripts/controllers/cartCtrl.js
@@ -1,21 +1,25 @@
 angular.module('sampleCartApp.controller').controller('cartCtrl', function( $scope, $rootScope, cartService ){
-	$scope.message = "View Your Cart!";
+	$scope.message = "View Your Cart";
 	//$scope.cart = $rootScope.cart || [];
+	$scope.loading = true;
 	cartService.getCart().then(function( response ){
 		//console.log( 'getCart :',response)
 		$rootScope.cart = response;
 		$scope.cart = response;
+		$scope.loading = false;
 	}, function(errorMessage){
 		console.log('getCart errorMessage : ',errorMessage);
 	});
 
 	$scope.updateCart = function( cart ){
+		$scope.loading = true;
 		cartService.updateCart( cart ).then(function( response ){
 			//console.log( 'updateCart :',response)
 			$rootScope.cart = response.cart;
 			$scope.cart = response.cart;
 			$scope.cartTotal = cartService.getTotal( $rootScope.cart );
 			$rootScope.alerts.push({type:"success", msg:  response.message });
+			$scope.loading = false;
 		}, function(errorMessage){
 			console.log('updateCart errorMessage : ',errorMessage);
 		});
